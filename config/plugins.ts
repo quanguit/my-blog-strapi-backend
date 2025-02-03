1df90bb18fce937fa4b08bdb989ebc48e719670c
Quang Do <nhatquang25012000@gmail.com>
2025-02-03 08:39:54 +0000

    feat: setup send email

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "quang250120000@gmail.com",
+        defaultReplyTo: "quang250120000@gmail.com",
+      },
+    },
+  },
+});
