7db6aaa2c53bc0be0a5b9d1bf4630a7791ea96dc
Quang Do <quangdo@192.168.1.9>
2026-01-20 12:13:40 +0700

    chore: remove

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -27,12 +27,4 @@ export default [
       },
     },
   },
-  {
-    name: "strapi::body",
-    config: {
-      jsonLimit: "50mb",
-      formLimit: "50mb",
-      textLimit: "50mb",
-    },
-  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -17,9 +17,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  upload: {
-    config: {
-      sizeLimit: 50 * 1024 * 1024, // 50MB
-    },
-  },
 });
